e15e2cb51753e66b4caaa34c323b2d87e5f46597
Darshan Chheda <ddc@somaiya.edu>
2019-12-26 01:23:30 +0530

    CLIST: future contests

---
+++ future.py
@@ -0,0 +1,62 @@
+#Import Dependencies
+import requests,ast, json
+from requests_oauthlib import OAuth1
+from datetime import datetime,date,time
+
+#URL - response
+url = "https://clist.by/api/v1/json/contest//?username=darshanc99&api_key=387ae2fdb36a9a632694f4d4bd8a5e9b35f7c4dc"
+payload = "{}"
+response = requests.request("GET",url,data=payload)
+a = response.text
+parsed_json = json.loads(a)
+total = parsed_json['meta']['total_count']
+total = str(total)
+j_count = int(total[:2])
+today = datetime.today().day
+toyear = datetime.today().year
+tomonth = datetime.today().month
+count = 0
+
+for j in range(j_count+1):
+  k = str(j*1000)
+  url = "https://clist.by/api/v1/json/contest/?limit="+total+"&offset="+k+"&username=darshanc99&api_key=387ae2fdb36a9a632694f4d4bd8a5e9b35f7c4dc"
+  payload = "{}"
+  response = requests.request("GET",url,data=payload)
+  print("OFFSET:",k,"\n","RESPONSE:",response,"\n\n")
+  a = response.text
+  parsed_json = json.loads(a)
+  if j == j_count:
+    print("INSIDE END")
+    for i in range(int(total) - int(k)):
+      startyear = int(parsed_json['objects'][i]['start'][:4])
+      startmonth = int(parsed_json['objects'][i]['start'][5:7])
+      startday = int(parsed_json['objects'][i]['start'][8:10])
+      if startyear > toyear:
+        print("Event:",i,"=",parsed_json['objects'][i])
+        count+=1
+      elif startyear == toyear:
+        if startmonth > tomonth:
+          print("Event:",i,"=",parsed_json['objects'][i])
+          count+=1
+        elif startmonth == tomonth:
+          if startday > today:
+            print("Event:",i,"=",parsed_json['objects'][i])
+            count+=1
+
+  else:
+    for i in range(1000):
+      startyear = int(parsed_json['objects'][i]['start'][:4])
+      startmonth = int(parsed_json['objects'][i]['start'][5:7])
+      startday = int(parsed_json['objects'][i]['start'][8:10])
+      if startyear > toyear:
+        print("Event:",i,"=",parsed_json['objects'][i])
+        count+=1
+      elif startyear == toyear:
+        if startmonth > tomonth:
+          print("Event:",i,"=",parsed_json['objects'][i])
+          count+=1
+        elif startmonth == tomonth:
+          if startday > today:
+            print("Event:",i,"=",parsed_json['objects'][i])
+            count+=1
+print(count)
